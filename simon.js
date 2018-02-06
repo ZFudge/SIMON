ea5253324afffa4784e2120f3d15e71c7f406258
Zach Fudge <zacheryfudge@gmail.com>
2018-02-06 09:30:24 -0700

    light states synced

--- simon.js
+++ simon.js
@@ -1,4 +1,5 @@
 const simon = {
+	active: false,
 	count: 0,
 	pattern: [],
 	clickPattern: [],
@@ -6,11 +7,13 @@ const simon = {
 	clickable: false,
 	strict: false,
 	strictSwitch() {
-		this.strict = !this.strict;
-		if (this.strict) {
-			document.getElementById('strict').style.color = '#F00'
-		} else {
-			document.getElementById('strict').style.color = '#333'
+		if (this.active) {
+			this.strict = !this.strict;
+			if (this.strict) {
+				document.getElementById('strict').style.color = '#F00'
+			} else {
+				document.getElementById('strict').style.color = '#333'
+			}
 		}
 	},
 	colorSoundTable: [
@@ -36,6 +39,7 @@ const simon = {
 			this.topElement_Sound[0].style.backgroundColor = this.colorSoundTable[index][0].dataset.background.split("!")[1];
 			this.topElement_Sound[1].pause();
 			this.topElement_Sound[1].currentTime = 0;
+			this.status.changeLight("green");
 		}, 500);
 	},
 	iterate(index = 0, turn = true) {
@@ -53,7 +57,7 @@ const simon = {
 							(index < this.pattern.length-1) ? simon.iterate(index + 1, turn) : (turn) ? this.turn():null;
 						}, this.iterationSpeed);
 					}
-			}, this.iterationSpeed); // 500
+			}, this.iterationSpeed);
 		}
 	},
 	restartable: true,
@@ -64,6 +68,9 @@ const simon = {
 		this.clickPattern = [];
 		this.iterationSpeed = 750;
 		this.clickable = false;
+		this.active = true;
+		simon.status.changeLight("on");
+		document.getElementById("count").style.color = "#EEE";
 		if (this.restartable) setTimeout(() => this.turn(), 750)
 	},
 	updateCount() {
@@ -75,16 +82,29 @@ const simon = {
 						
 	},
 	won() {
+		if (this.hard) this.hardMode();
 		this.colorSoundTable[6].play();
-		setTimeout(()=>this.restart(),6500);
-		if (this.hard) {
-			this.hardMode();
-			this.hardMode();
-		}
+		setTimeout(() => {
+			this.status.changeLight("off");
+			this.active = false;
+			this.hard = false;
+			this.count = -1;
+			this.updateCount();
+			document.getElementById("count").style.color = "#161616";
+		}, 6500);
 	},
 	lost() {
 		this.colorSoundTable[7].play();
-		setTimeout(()=>this.restart(),3500);
+		this.status.changeLight("red")
+		let red = false;
+		let loop = setInterval(() => {
+			(red) ? this.status.changeLight("red") : this.status.changeLight("off");
+			red = !red;
+		}, 500);
+		setTimeout(()=>{
+			this.restart();
+			clearInterval(loop);
+		},3500);
 	},
 	click(btn) {
 		if (this.pattern.length && this.clickable) {
@@ -99,12 +119,13 @@ const simon = {
 					this.pattern[this.clickPattern.length][0].style.backgroundColor = this.pattern[this.clickPattern.length][0].dataset.background.split("!")[1];
 					this.clickPattern.push(this.topElement_Sound);
 					if (this.clickPattern.length === this.pattern.length) {
+						simon.status.changeLight("on");
 						this.clickable = false;
 						this.clickPattern = [];
 						this.updateCount();
 						this.checkIterationSpeed();
 						this.colorSoundTable[5].play();
-						(this.count < 20) ? setTimeout(()=>this.iterate(), this.iterationSpeed) : this.won();
+						(this.count < 20) ? setTimeout(()=>{this.iterate();this.status.changeLight("on")}, this.iterationSpeed) : this.won();
 					}
 				};
 			} else {
@@ -113,8 +134,10 @@ const simon = {
 				this.lost();
 			} else {
 				this.colorSoundTable[4].play();
+				simon.status.changeLight("red");
 				this.clickPattern = [];
 				setTimeout(()=> {
+					simon.status.changeLight("on");
 					this.colorSoundTable[4].pause();
 					this.colorSoundTable[4].currentTime = 0;
 					(this.strict) ? this.lost() : this.iterate(0,false); 
@@ -126,9 +149,9 @@ const simon = {
 	status: {
 		light: document.getElementById("status"),
 		off: ["#444", "inset 0 -2px 3px 0 #222, 0 0 10px 3px #222"],
-		on: ["#DDD", "inset 0 -2px 3px 0 #BBB, 0 0 10px 3px #BBB"],
-		green: ["#0F0", "inset 0 -2px 10px 0 #050, inset 0 2px 10px 0 #FFF, 0 0 10px 3px #050"],
-		red: ["#F00", "inset 0 -2px 10px 0 #500, inset 0 2px 10px 0 #FFF, 0 0 10px 3px #500"],
+		on: ["#DDD", "inset 0 -2px 3px 0 #BBB, 0 0 10px 3px #AAA"],
+		green: ["#0F0", "inset 0 -2px 10px 0 #0F0, inset 0 2px 10px 0 #CFC, 0 0 10px 3px #0D0"],
+		red: ["#F00", "inset 0 -2px 10px 0 #F00, inset 0 2px 10px 0 #FCC, 0 0 10px 3px #D00"],
 		changeLight(setting) {
 			this.light.style.backgroundColor = simon.status[setting][0];
 			this.light.style.boxShadow = simon.status[setting][1];
@@ -136,27 +159,30 @@ const simon = {
 	},
 	hard: false,
 	hardMode() {
-		if (this.hard) {
-			this.hard = false;
-			document.getElementById("simon").style.transitionDuration = "0s";
-			document.getElementById("round").style.transitionDuration = "0s";
-			this.rotation = 0;
-		} else {
-			if (!this.pattern.length) this.turn();
-			this.hard = true;
-			if (!this.strict) this.strictSwitch();
-			this.hardLoop();
+		if (this.active) {
+			if (this.hard) {
+				this.hard = false;
+				document.getElementById("simon").style.transitionDuration = "0s";
+				document.getElementById("round").style.transitionDuration = "0s";
+				this.rotation = 0;
+			} else {
+				if (!this.pattern.length) this.turn();
+				this.hard = true;
+				this.hardLoop();
+			}
 		}
 	},
 	hardLoop() {
-		const speed = this.iterationSpeed * 36;
-		document.getElementById("simon").style.transitionDuration = speed/1000 + "s";
-		document.getElementById("round").style.transitionDuration = speed/1000 + "s";
-		this.rotation = 1;
-		setTimeout(() => {
-		    this.rotation = -1;
-		    setTimeout(()=>this.hardLoop(), speed);
-		}, speed);
+		if (this.active) {
+			const speed = this.iterationSpeed * 36;
+			document.getElementById("simon").style.transitionDuration = speed/1000 + "s";
+			document.getElementById("round").style.transitionDuration = speed/1000 + "s";
+			this.rotation = 1;
+			setTimeout(() => {
+			    this.rotation = -1;
+			    setTimeout(()=>this.hardLoop(), speed);
+			}, speed);
+		}
 	},
 	set rotation(n) {
 		document.getElementById("simon").style.transform = "rotate(" + n + "turn)";
